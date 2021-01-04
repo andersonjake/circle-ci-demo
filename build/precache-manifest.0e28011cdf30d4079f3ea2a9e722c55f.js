80378e4072906a72b34f3151c7ca070759e048df
Jake <jakeanderson927@gmail.com>
2021-01-04 16:50:33 -0700

    adding hosting to proejct

---
+++ build/precache-manifest.0e28011cdf30d4079f3ea2a9e722c55f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb4a3e1a573eb253af683823eb56e37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a9658e35ec36f2c784e",
+    "url": "/static/css/main.e120a995.chunk.css"
+  },
+  {
+    "revision": "df4782af7a013537be51",
+    "url": "/static/js/2.2bcb2c04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2bcb2c04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a9658e35ec36f2c784e",
+    "url": "/static/js/main.11e33ea2.chunk.js"
+  },
+  {
+    "revision": "121f45b245d7c360cd10",
+    "url": "/static/js/runtime-main.a051cd10.js"
+  }
+]);
